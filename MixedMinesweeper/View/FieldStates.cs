3592991d9741ec7979365fc28fb413871571bab0
Stweber <webersteven93@gmail.com>
2015-11-20 18:19:24 +0100

    Umzug, neue Dateien

---
+++ MixedMinesweeper/View/FieldStates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MixedMinesweeper.View
+{
+    public enum FieldStates : int
+    {
+        Covered = 0,
+        RedFlag = 1,
+        BlueFlag = 2,
+        GreenFlag = 3,
+        YellowFlag = 4,
+        QuestioMark = 5,
+        Uncoverd = 6
+    }
+}
