932be090976c40369929c8a9486e9cd593488d4b
Stweber <webersteven93@gmail.com>
2015-11-21 17:09:17 +0100

    Felder kennen auf Control-Ebene ihre Nachbarn
    Die einzelnen Controls beinhalten nun nicht mehr nur indirekt die
    Nachbarn im Field, sondern auch die physischen Controls dazu.

--- MixedMinesweeper/View/CtrlFieldCheckBox.cs
+++ MixedMinesweeper/View/CtrlFieldCheckBox.cs
@@ -1,6 +1,7 @@
 ﻿using MixedMinesweeper.Model;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -9,16 +10,62 @@ namespace MixedMinesweeper.View
 {
     public class CtrlFieldCheckBox : CheckBox
     {
+        private Image _BackgroundImg;
+
         private FieldStates _FieldState = FieldStates.Covered;
         public FieldStates FieldState
         {
             get { return _FieldState; }
         }
 
+        private int _NeighbouringMines = 0;
+        public int NeighbouringMines
+        {
+            get { return _NeighbouringMines; }
+        }
+
+        private List<CtrlFieldCheckBox> _NeighbouringFields = new List<CtrlFieldCheckBox>();
+        public List<CtrlFieldCheckBox> NeighbouringFields
+        {
+            get { return _NeighbouringFields; }
+            set
+            {
+                this._NeighbouringFields = value;
+                foreach (CtrlFieldCheckBox field in NeighbouringFields)
+                {
+                    if (field.Field.IsMine)
+                    {
+                        this._NeighbouringMines++;
+                    }
+                }
+            }
+        }
+
         private Field _Field;
         public Field Field
         {
             get { return _Field; }
+        }
+
+        public void CoverupNeighbours()
+        {
+            this.SetBackgroundImage();
+            if (!this.Field.IsMine)
+            {
+                this.Enabled = false;
+                if (!this.Checked)
+                {
+                    this.Text = this.NeighbouringMines.ToString();
+                    this.Checked = true;
+                    if (this.NeighbouringMines == 0)
+                    {
+                        foreach (CtrlFieldCheckBox item in this.NeighbouringFields)
+                        {
+                            item.CoverupNeighbours();
+                        }
+                    }
+                }
+            }
         }
 
         public CtrlFieldCheckBox(Field field)
@@ -30,47 +77,109 @@ namespace MixedMinesweeper.View
             base.Appearance = Appearance.Button;
             base.Height = 40;
             base.Width = 40;
+            base.TextAlign = ContentAlignment.MiddleCenter;
+            base.Font = new System.Drawing.Font("Consolas", 12, FontStyle.Bold);
+            base.ForeColor = Color.FromKnownColor(System.Drawing.KnownColor.Black);
+            base.Margin = new Padding(0);
+            base.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            base.FlatAppearance.CheckedBackColor = Color.FromKnownColor(System.Drawing.KnownColor.Black);
             this._Field = field;
-            base.MouseClick += CtrlFieldCheckBox_MouseClick;
+            base.MouseDown += CtrlFieldCheckBox_MouseDown;
+
+            this.LoadImage();
         }
 
-        void CtrlFieldCheckBox_MouseClick(object sender, MouseEventArgs e)
+        private void LoadImage()
+        {
+            string file = "field_";
+            if (this.Field.IsMine)
+            {
+                file += this.Field.Color.ToString().ToLower();
+            }
+            else
+            {
+                if (this.Field.GetColoredMines(MinesColors.Red) > 0)
+                {
+                    file += "red";
+                }
+                if (this.Field.GetColoredMines(MinesColors.Blue) > 0)
+                {
+                    file += "blue";
+                }
+                if (file == "field_")
+                {
+                    file = "field";
+                }
+            }
+
+            System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.IO.Stream myStream = myAssembly.GetManifestResourceStream("MixedMinesweeper.Images." + file + ".png");
+            Bitmap image = new Bitmap(myStream);
+            this._BackgroundImg = image;
+        }
+
+        void CtrlFieldCheckBox_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
+                //this.Checked = true;
+                //this.Enabled = false;
+
                 if (this._Field.IsMine)
                 {
                     // loser
-                    switch (this._Field.Color)
-                    {
-                        case MinesColors.NoColor:
-                            throw new Exception("no color but is mine?");
-                        case MinesColors.Red:
-                            break;
-                        case MinesColors.Green:
-                            break;
-                        case MinesColors.Blue:
-                            break;
-                        case MinesColors.Yellow:
-                            break;
-                        default:
-                            break;
-                    }
+                    this.Text = "*";
                 }
                 else
                 {
-                    this.IncrementFieldState();
+                    this.CoverupNeighbours();
                 }
             }
             else if (e.Button == System.Windows.Forms.MouseButtons.Right)
             {
-
+                this.IncrementFieldState();
             }
+        }
+
+        private void SetBackgroundImage()
+        {
+            
+            base.BackgroundImage = this._BackgroundImg;
+            base.BackgroundImageLayout = ImageLayout.Stretch;
+
         }
 
         private void IncrementFieldState()
         {
-            this._FieldState = ((int)this._FieldState == Enum.GetValues(typeof(FieldStates)).Length - 1) ? 0 : this._FieldState++;
+            if (!this.Checked)
+            {
+                this._FieldState = ((int)this._FieldState == Enum.GetValues(typeof(FieldStates)).Length - 1) ? 0 : ++this._FieldState;
+                switch (this._FieldState)
+                {
+                    case FieldStates.Covered:
+                        break;
+                    case FieldStates.RedFlag:
+                        this.Text = "R";
+                        break;
+                    case FieldStates.BlueFlag:
+                        this.Text = "B";
+                        break;
+                    case FieldStates.GreenFlag:
+                        this.Text = "G";
+                        break;
+                    case FieldStates.YellowFlag:
+                        this.Text = "Y";
+                        break;
+                    case FieldStates.QuestioMark:
+                        this.Text = "?";
+                        break;
+                    case FieldStates.Uncoverd:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
 
 
--- MixedMinesweeper/View/FormMineField.cs
+++ MixedMinesweeper/View/FormMineField.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MixedMinesweeper.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,14 +12,94 @@ namespace MixedMinesweeper.View
 {
     public partial class FormMineField : Form
     {
-        public FormMineField()
+        private DateTime _Time;
+
+        private MineField _MineField;
+        public MineField MineField
+        {
+            get { return _MineField; }
+        }
+
+
+        public FormMineField(MineField field)
         {
             InitializeComponent();
+            if (field == null)
+            {
+                throw new ArgumentNullException("field");
+            }
+            this._MineField = field;
+            this._Time = DateTime.Now;
         }
 
         private void FormMineField_Load(object sender, EventArgs e)
         {
-         
+            int heigth = this._MineField.Fields.GetLength(1);
+            int width = this._MineField.Fields.GetLength(0);
+            CtrlFieldCheckBox[,] fields = new CtrlFieldCheckBox[width, heigth];
+            this.flowLayoutPanel_MineField.Width = width * 42;
+            this.flowLayoutPanel_MineField.Height = heigth * 42;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < heigth; y++)
+                {
+                    fields[x, y] = new CtrlFieldCheckBox(this._MineField.Fields[x, y]);
+                    this.flowLayoutPanel_MineField.Controls.Add(fields[x, y]);
+                }
+            }
+
+            for (int x = 0; x < fields.GetLength(0); x++)
+            {
+                for (int y = 0; y < fields.GetLength(1); y++)
+                {
+                    // First line (in a square of 3...)
+                    List<CtrlFieldCheckBox> neighbours = new List<CtrlFieldCheckBox>();
+                    if (x - 1 >= 0)
+                    {
+                        if (y - 1 >= 0)
+                        {
+                            neighbours.Add(fields[x - 1, y - 1]);
+                        }
+                        neighbours.Add(fields[x - 1, y]);
+                        if (y + 1 < this.MineField.Fields.GetLength(1))
+                        {
+                            neighbours.Add(fields[x - 1, y + 1]);
+                        }
+                    }
+
+                    // second line
+                    if (y - 1 >= 0)
+                    {
+                        neighbours.Add(fields[x, y - 1]);
+                    }
+                    if (y + 1 < this.MineField.Fields.GetLength(1))
+                    {
+                        neighbours.Add(fields[x, y + 1]);
+                    }
+
+                    // third line
+                    if (x + 1 < this.MineField.Fields.GetLength(0))
+                    {
+                        if (y - 1 >= 0)
+                        {
+                            neighbours.Add(fields[x + 1, y - 1]);
+                        }
+                        neighbours.Add(fields[x + 1, y]);
+                        if (y + 1 < this.MineField.Fields.GetLength(1))
+                        {
+                            neighbours.Add(fields[x + 1, y + 1]);
+                        }
+                    }
+                    fields[x, y].NeighbouringFields = neighbours;
+                }
+            }
+            //for (int x = 0; x < this._MineField.Fields.GetLength(0); x++)
+            //{
+            //    for (int y = 0; y < this._MineField.Fields.GetLength(1); y++)
+            //    {
+            //    }
+            //}
         }
 
         private void checkBox1_MouseDown(object sender, MouseEventArgs e)
@@ -31,5 +112,12 @@ namespace MixedMinesweeper.View
                 (sender as CheckBox).Text = i.ToString();
             }
         }
+
+        private void timePlayingTime_Tick(object sender, EventArgs e)
+        {
+            TimeSpan played = DateTime.Now - this._Time;
+            this.lblTime.Text = played.Minutes + ":" + played.Seconds;
+        }
+
     }
 }
