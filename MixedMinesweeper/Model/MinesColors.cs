c0fed5c22533d86d565ff937d520eb0bc2895809
Stweber <webersteven93@gmail.com>
2015-11-21 17:09:56 +0100

    Namensänderung

---
+++ MixedMinesweeper/Model/MinesColors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MixedMinesweeper.Model
+{
+    public enum MinesColors
+    {
+        NoColor, Red, Green, Blue, Yellow
+    }
+}
