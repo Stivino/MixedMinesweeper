5c4ad17754488a93ac085e5bcbc4bb7e92947550
Stweber <webersteven93@gmail.com>
2015-11-24 18:46:47 +0100

    Kommentierung und Bugfix.
    Der Feldstatus wird nun korrekt inkrementiert.

--- MixedMinesweeper/View/CtrlFieldCheckBox.cs
+++ MixedMinesweeper/View/CtrlFieldCheckBox.cs
@@ -12,62 +12,62 @@ namespace MixedMinesweeper.View
     {
         private Image _BackgroundImg;
 
-        private FieldStates _FieldState = FieldStates.Covered;
+        private FieldStates _FieldState = FieldStates.Default;
+        /// <summary>
+        /// Gibt den aktuellen, durch den Rechtsklick festgelegten Status des Feldes zurück.
+        /// </summary>
+        /// <remarks>
+        /// Wenn die Checked-Eigenschaft true ist, greift diese Eigenschaft nicht mehr. 
+        /// </remarks>
         public FieldStates FieldState
         {
             get { return _FieldState; }
         }
 
         private int _NeighbouringMines = 0;
+        /// <summary>
+        /// Gibt die Anzahl der angrenzenden Minen des unterliegenden Feldes zurück.
+        /// </summary>
         public int NeighbouringMines
         {
             get { return _NeighbouringMines; }
         }
 
         private List<CtrlFieldCheckBox> _NeighbouringFields = new List<CtrlFieldCheckBox>();
+        /// <summary>
+        /// Gets oder sets the neighbouring fields as CtrlFieldCheckbox controls.
+        /// </summary>
         public List<CtrlFieldCheckBox> NeighbouringFields
         {
             get { return _NeighbouringFields; }
             set
             {
                 this._NeighbouringFields = value;
+                int tmpMines = 0;
                 foreach (CtrlFieldCheckBox field in NeighbouringFields)
                 {
                     if (field.Field.IsMine)
                     {
-                        this._NeighbouringMines++;
+                        tmpMines++;
                     }
                 }
+                this._NeighbouringMines = tmpMines;
             }
         }
 
         private Field _Field;
+        /// <summary>
+        /// Gibt das Feld an, welches durch diese Instanz repräsentiert wird.
+        /// </summary>
         public Field Field
         {
             get { return _Field; }
         }
 
-        public void CoverupNeighbours()
-        {
-            this.SetBackgroundImage();
-            if (!this.Field.IsMine)
-            {
-                this.Enabled = false;
-                if (!this.Checked)
-                {
-                    this.Text = this.NeighbouringMines.ToString();
-                    this.Checked = true;
-                    if (this.NeighbouringMines == 0)
-                    {
-                        foreach (CtrlFieldCheckBox item in this.NeighbouringFields)
-                        {
-                            item.CoverupNeighbours();
-                        }
-                    }
-                }
-            }
-        }
-
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="field">Gibt das zu repräsentierende Feld an.</param>
         public CtrlFieldCheckBox(Field field)
         {
             if (field == null)
@@ -89,6 +89,36 @@ namespace MixedMinesweeper.View
             this.LoadImage();
         }
 
+        /// <summary>
+        /// Weist das Feld an, sich selbst zu öffnen und wenn die NeighbouringMines Eigenschaft 0 ist auch die Nachbarn zu öffnen.
+        /// </summary>
+        /// <remarks>
+        /// Über die Instanzgrenze hinweg erfolgt der Aufruf rekursiv.</remarks>
+        public void CoverupNeighbours()
+        {
+            this.SetBackgroundImage();
+            if (!this.Field.IsMine)
+            {
+                this.Enabled = false;
+                if (!this.Checked)
+                {
+                    this.Text = this.NeighbouringMines.ToString();
+                    this.Checked = true;
+                    if (this.NeighbouringMines == 0)
+                    {
+                        foreach (CtrlFieldCheckBox item in this.NeighbouringFields)
+                        {
+                            item.CoverupNeighbours();
+                        }
+                    }
+                }
+            }
+        }
+
+       
+        /// <summary>
+        /// Lädt das Hintergrundbild in Abhängigkeit zu den angrenzenden Minenfarben.
+        /// </summary>
         private void LoadImage()
         {
             string file = "field_";
@@ -118,7 +148,12 @@ namespace MixedMinesweeper.View
             this._BackgroundImg = image;
         }
 
-        void CtrlFieldCheckBox_MouseDown(object sender, MouseEventArgs e)
+        /// <summary>
+        /// Handled das MouseDown event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Enthält Informationen über die gedrückte Taste.</param>
+        private void CtrlFieldCheckBox_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
@@ -141,14 +176,18 @@ namespace MixedMinesweeper.View
             }
         }
 
+        /// <summary>
+        /// Setzt das Hintergrund entsprechend dem geladenem Bild.
+        /// </summary>
         private void SetBackgroundImage()
         {
-            
             base.BackgroundImage = this._BackgroundImg;
             base.BackgroundImageLayout = ImageLayout.Stretch;
-
         }
 
+        /// <summary>
+        /// Inkrementiert den aktuellen Feldstatus.
+        /// </summary>
         private void IncrementFieldState()
         {
             if (!this.Checked)
@@ -156,8 +195,6 @@ namespace MixedMinesweeper.View
                 this._FieldState = ((int)this._FieldState == Enum.GetValues(typeof(FieldStates)).Length - 1) ? 0 : ++this._FieldState;
                 switch (this._FieldState)
                 {
-                    case FieldStates.Covered:
-                        break;
                     case FieldStates.RedFlag:
                         this.Text = "R";
                         break;
@@ -173,9 +210,8 @@ namespace MixedMinesweeper.View
                     case FieldStates.QuestioMark:
                         this.Text = "?";
                         break;
-                    case FieldStates.Uncoverd:
-                        break;
                     default:
+                        this.Text = "";
                         break;
                 }
             }
